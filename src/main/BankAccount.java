efd5b41d211c3ecab3e9ab4b1aff06f4cc362c67
Orly Stulbach <z5676717@ad.unsw.edu.au>
2026-03-24 14:13:41 -0500

    tests and main for transaction history task

--- src/main/BankAccount.java
+++ src/main/BankAccount.java
@@ -1,16 +1,22 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankAccount {
 
     private double balance;
+    private List<String> transactionHistory;
 
     public BankAccount() {
         this.balance = 0;
+        this.transactionHistory = new ArrayList<>();
     }
 
     public void deposit(double amount) {
         if(amount > 0) {
             this.balance += amount;
+            this.transactionHistory.add("Deposited: " + amount);
         } else {
             throw new IllegalArgumentException();
         }
@@ -19,4 +25,9 @@ public class BankAccount {
     public double getBalance() {
         return this.balance;
     }
+
+    public List<String> transactionHistory() {
+        return this.transactionHistory;
+    }
+
 }
--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MainMenu {
 
     private static final int EXIT_SELECTION = 2;
-	private static final int MAX_SELECTION = 2;
+	private static final int MAX_SELECTION = 3;
 
 	private BankAccount userAccount;
     private Scanner keyboardInput;
@@ -20,6 +20,7 @@ public class MainMenu {
         
         System.out.println("1. Make a deposit");
         System.out.println("2. Exit the app");
+        System.out.println("3. View transaction history");
 
     }
 
@@ -36,6 +37,10 @@ public class MainMenu {
         switch (selection) {
             case 1:
                 performDeposit();
+                break;
+            case 3:
+                performTransactionHistory();
+                break;
         }
     }
 
@@ -46,6 +51,10 @@ public class MainMenu {
             depositAmount = keyboardInput.nextInt();
         }
         userAccount.deposit(depositAmount);
+    }
+
+    public void performTransactionHistory() {
+        System.out.println(userAccount.transactionHistory());
     }
 
     public void run() {
