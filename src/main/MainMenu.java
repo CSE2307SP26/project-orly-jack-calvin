22b2ef9eabb48d86db20ad7b834ef6203c8aaa75
calvinkapral <ckapral@outlook.com>
2026-03-25 14:04:26 -0500

    added balance feature

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class MainMenu {
 
-    private static final int EXIT_SELECTION = 2;
-	private static final int MAX_SELECTION = 2;
+    private static final int EXIT_SELECTION = 3;
+	private static final int MAX_SELECTION = 3;
 
 	private BankAccount userAccount;
     private Scanner keyboardInput;
@@ -19,7 +19,8 @@ public class MainMenu {
         System.out.println("Welcome to the 237 Bank App!");
         
         System.out.println("1. Make a deposit");
-        System.out.println("2. Exit the app");
+        System.out.println("2. Check Balance");
+        System.out.println("3. Exit the app");
 
     }
 
@@ -36,6 +37,14 @@ public class MainMenu {
         switch (selection) {
             case 1:
                 performDeposit();
+                break;
+
+            case 2:
+                checkBalance();
+                break;
+            case 3:
+                System.out.println("Goodbye!");
+                break;
         }
     }
 
@@ -46,6 +55,10 @@ public class MainMenu {
             depositAmount = keyboardInput.nextInt();
         }
         userAccount.deposit(depositAmount);
+    }
+
+    public void checkBalance(){
+        System.out.println("Current balance " + userAccount.getBalance());
     }
 
     public void run() {
