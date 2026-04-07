e017cca6c908d9f96d2317c77774c64f09ca5593
Orly Stulbach <z5676717@ad.unsw.edu.au>
2026-04-07 16:15:14 -0500

    changed while main menu to if statement

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -359,15 +359,13 @@ public class MainMenu {
     public void run() {
         int selection = -1;
         while(selection != EXIT_SELECTION) {
-            // displayOptions();
-            while (mainMenu) {
+            if (mainMenu) {
                 mainMenuDisplayOptions();
                 selection = getUserSelection(MAIN_MENU_EXIT_SELECTION);
                 // processInput(selection);
                 processMenuInput(selection);
             }
-            // else {
-            if (userDisplay) {
+            else if (userDisplay) {
                 displayOptions();
                 selection = getUserSelection(MAX_SELECTION);
                 processInput(selection);
@@ -377,7 +375,6 @@ public class MainMenu {
                 selection = getUserSelection(ADMIN_EXIT_SELECTION);
                 processAdministratorInput(selection);
             }
-            // }
             
         }
     }
