79e4e6bcf5b5141363ee0c551a3b81849ae77639
Orly Stulbach <z5676717@ad.unsw.edu.au>
2026-04-21 16:38:42 -0500

    test for renaming accounts

---
+++ src/test/RenameAccountTest.java
@@ -0,0 +1,28 @@
+package test;
+import main.BankAccount;
+import main.Bank;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RenameAccountTest {
+  
+  private BankAccount testAccount;
+
+  @BeforeEach
+  public void setup() {
+    this.testAccount = new BankAccount();
+  }
+  
+  @Test
+  public void renameAccountTest() {
+
+    String newName = "new_name";
+    testAccount.renameAccount(testAccount, newName);
+    assertEqual(testAccount.getName(), newName);
+
+  }
+}
